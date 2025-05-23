a7ace3bfc0d84a3a5abf596d0eac7ad1cf2240b5
Arief Yusron <ariefyusron98@gmail.com>
2025-05-23 21:19:23 +0700

    zustand persist

--- src/store/todo.js
+++ src/store/todo.js
@@ -1,8 +1,12 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
 
-const store = create((set) => ({
+const store = create(persist((set) => ({
   todoList: [],
   addTodo: (todo) => set((state) => ({ todoList: [todo, ...state.todoList] }))
+}),{
+  name: 'store-todo',
+  storage: createJSONStorage(() => localStorage)
 }));
 
 export default store;
